415dfa56bf0e582eb5edbc6823f3ca17e421a152
Kochi_Takuto <68525205+KochiTakuto@users.noreply.github.com>
2022-06-26 21:18:47 +0900

    スプレット構文を学習

--- src/index.js
+++ src/index.js
@@ -136,3 +136,39 @@
 /**
  * スプレット構文...
  */
+//配列の展開(配列の要素が先頭から展開される)
+//const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる(配列の要素をまとめて受け取れる)
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合(同じ配列名を指定して同じ値をコピー、参照渡ししない！！)
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr4);
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// =でやると参照渡しになり、arr4まで変わってしまう
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
