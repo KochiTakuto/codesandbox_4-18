492cb206419741ed2da1a2e60c6ec29500f5a40e
Kochi_Takuto <68525205+KochiTakuto@users.noreply.github.com>
2022-06-26 21:42:31 +0900

    mapとfilterを学習

--- src/index.js
+++ src/index.js
@@ -172,3 +172,36 @@
 /**
  * mapやfilterを使った配列の処理
  */
+//const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// map(配列の値をreturnで返す)
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr);
+
+//第一引数だけでなく、第二引数も取れる
+//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// filter(returnの後に条件を記入する。記入した条件だけ返す)
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
